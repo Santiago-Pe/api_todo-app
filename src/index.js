18f6dd8a99500ee412804145ff12c4dbbeb2f06d
Santiago-Pe <penasantiago364@gmail.com>
2023-04-30 22:04:30 -0300

    add mongoose, mongodb and dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+/* ---------- Requires ----------*/
 const express = require('express');
+const mongoose = require('monoogse');
 
+/* ---------- Consts ??? ----------*/
 const app = express();
 const port = process.env.PORT || 9000;
 
+/* ---------- Routes ----------*/
+app.get('/', (req, res) => {
+    res.send('welocme to my api')
+})
+
+/* ---------- Mongo db conection ----------*/
+mongoose.connect()
+
+/* ---------- ???? ----------*/
 app.listen(port, () => console.log('server listening on port: ', port));
